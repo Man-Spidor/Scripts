be08ddf1b2ddc7f1d4737ecfaa820755bee8c364
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-12 19:50:21 -0500

    added grass logic (moo)

--- Encounters.cs
+++ Encounters.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Encounters : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public LayerMask grassLayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D() {
+        if(Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer)) {
+            if(Random.Range(1, 101) <= 10) Debug.Log("Encountered a wild MOM! OoO");
+        }
     }
 }
