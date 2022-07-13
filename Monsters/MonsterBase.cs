4722df3e9779589ce9e4c612189b85dcc1da0b3b
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-13 18:29:07 -0500

    ?

--- Monsters/MonsterBase.cs
+++ Monsters/MonsterBase.cs
@@ -22,6 +22,50 @@ public class MonsterBase : ScriptableObject
     [SerializeField] int spAttack;
     [SerializeField] int spDefense;
     [SerializeField] int speed;
+
+    public string getName() {
+        return name; 
+    }
+
+    public string getDesc() {
+        return description;
+    }
+
+    public Sprite getFro() {
+        return frontSprite;
+    }
+
+    public Sprite getBack() {
+        return backSprite;
+    }
+
+    public Classes getClass() {
+        return creatureClass;
+    }
+
+    public int getMax() {
+        return maxHp;
+    }
+
+    public int getAtt() {
+        return attack;
+    }
+
+    public int getDef() {
+        return defense;
+    }
+
+    public int getSpA() {
+        return spAttack;
+    }
+
+    public int getspD() {
+        return spDefense;
+    }
+
+    public int getSpe() {
+        return speed;
+    }
 }
 
 public enum Classes {
