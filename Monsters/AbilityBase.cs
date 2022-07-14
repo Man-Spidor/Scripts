2564f47f04b97c1203b14539fa73a763b85a77be
PIC <ocampo.mjesus@gmail.com>
2022-07-14 18:22:19 -0500

    [CreateAssetMenu(fileName = "Monster", menuName =

--- Monsters/AbilityBase.cs
+++ Monsters/AbilityBase.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Monster", menuName = "Monster/Create New Monster")]
+
 public class AbilityBase : ScriptableObject
 {
     [SerializeField] string name;
