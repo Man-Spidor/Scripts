27c036c547e11d8aee3af7bde270c7b1be309ea5
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-15 10:53:36 -0500

    based monster

--- Battle/BattleHUD.cs
+++ Battle/BattleHUD.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TMPro;
+using TMPro; 
 
 public class BattleHUD : MonoBehaviour
 {
-    [SerializeField]  nameText;
+    [SerializeField] TextMeshPro nameText;
+    [SerializeField] TextMeshPro levelText;
+    [SerializeField] HPBar hpBar;
 
+    public void setData(Monster monster) {
+        nameText.text = monster.Base.getName();
+        levelText.text = "Lvl " + monster.Level;
+        hpBar.setHP(monster.HP / monster.maxHp);
+    }
 }
--- Monsters/Monster.cs
+++ Monsters/Monster.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Monsters
 {
-    MonsterBase _base;
-    int level;
+    public MonsterBase Base { get; set; }
+    public int Level {get; set; }
 
     public int HP { get; set; }
     public List<Ability> Abilities { get; set; }
 
     void Monster(MonsterBase pBase, int pLevel) {
-        _base = pBase;
-        level = pLevel;
-        HP = _base.getMax();
+        Base = pBase;
+        Level = pLevel;
+        HP = getHP();
 
         Abilities = new List<Ability>();
-        foreach (var ability in _base.GetAbilities()) {
-            if(ability.getLevel() <= level)  Abilities.Add(new Ability(ability.GetBase()));
+        foreach (var ability in Base.GetAbilities()) {
+            if(ability.getLevel() <= Level)  Abilities.Add(new Ability(ability.GetBase()));
 
             if(Abilities.Count >= 4) break;
         }
     }
 
     public int Attack() {
-        return _base.getAtt() * level;
+        return Base.getAtt() * Level;
+    }
+
+    public int getHP() {
+        return Base.getMax();
     }
 }
