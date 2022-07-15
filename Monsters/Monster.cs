d8c1c806356a4615fc4933be40665af6dffdfc88
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-14 19:44:25 -0500

    Velez	3125394447	Elijahryanv@gmail.com	Illinois

--- Monsters/Monster.cs
+++ Monsters/Monster.cs
@@ -7,11 +7,13 @@ public class Monsters
     MonsterBase _base;
     int level;
 
+    public int HP { get; set; }
     public List<Ability> Abilities { get; set; }
 
     void Monster(MonsterBase pBase, int pLevel) {
         _base = pBase;
         level = pLevel;
+        HP = _base.getMax();
 
         Abilities = new List<Ability>();
         foreach (var ability in _base.GetAbilities()) {
