7017af60de51b080d2f747b51704b8f5be4fac37
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-15 10:25:40 -0500

    for the lulz

--- Battle/HPBar.cs
+++ Battle/HPBar.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class HPBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject health;
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void setHP(float HP = 0.5f) {
         
+        health.transform.localScale = new Vector3(HP, 1f);
     }
 }
