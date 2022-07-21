a39f068662a5730250a2c1e8c60821ed96ddb849
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-21 11:17:06 -0500

    AddedBattleButtonControllerLMAO

---
+++ Battle/ButtonScripts/BattleButtonController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleButtonController : MonoBehaviour
+{
+    public void fightClicked() {
+        Debug.Log("Fight");
+    }
+
+    public void runClicked() {
+        Debug.Log("Fight");
+    }
+
+    public void move1Clicked() {
+        Debug.Log("Move1");
+    }
+    
+    public void move2Clicked() {
+        Debug.Log("Move2");
+    }
+    
+    public void move3Clicked() {
+        Debug.Log("Move3");
+    }
+    
+    public void move4Clicked() {
+        Debug.Log("Move4");
+    }
+}
