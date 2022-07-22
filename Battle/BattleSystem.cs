ae845e07a024dab0da340d7a276a7014c946f2d3
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-22 03:16:22 -0500

    deleted button and moved it all to battuh system also added detail text to move1

--- Battle/BattleSystem.cs
+++ Battle/BattleSystem.cs
@@ -28,6 +28,7 @@ public class BattleSystem : MonoBehaviour
         enemyHUD.setData(enemyUnit.monster);
 
         dialogueBox.setAbilityNames(playerUnit.monster.Abilities);
+        setDetails();
 
         yield return StartCoroutine(dialogueBox.typeDialogue($"A Wild {enemyUnit.monster.Base.name} Appeared"));
         yield return new WaitForSeconds(1f);
@@ -39,5 +40,42 @@ public class BattleSystem : MonoBehaviour
         state = BattleState.PlayerAction;
         StartCoroutine(dialogueBox.typeDialogue("Choose an action"));
         dialogueBox.EnableActionSelector(true);
+    }
+
+    //Button Functionalities
+    List<Ability> abilities;
+    private bool clicked = false;
+    
+    private void setDetails() {
+        abilities = playerUnit.monster.Abilities;
+    }
+    public void fightClicked() {
+        dialogueBox.EnableaAbilitySelector(true);
+        dialogueBox.EnableActionSelector(false);
+        dialogueBox.EnableDialogueText(false);
+    }
+
+    public void runClicked() {
+        Debug.Log("Run");
+    }
+
+    public void move1Clicked() {
+        clicked = !clicked;
+        if(!clicked) {
+            Debug.Log("Move1");
+        }
+        dialogueBox.setDetailText(abilities[0].Base.getDesc(), abilities[0].Base.getCool());
+    }
+    
+    public void move2Clicked() {
+        Debug.Log("Move2");
+    }
+    
+    public void move3Clicked() {
+        Debug.Log("Move3");
+    }
+    
+    public void move4Clicked() {
+        Debug.Log("Move4");
     }
 }
