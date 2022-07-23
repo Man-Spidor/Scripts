5c7e61c0795ee5ef78034cf1189c89710a930398
LOPTOP <ocampo.mjesus@gmail.com>
2022-07-23 01:05:15 -0500

    made button funcs their own class

--- Battle/BattleSystem.cs
+++ Battle/BattleSystem.cs
@@ -28,7 +28,8 @@ public class BattleSystem : MonoBehaviour
         enemyHUD.setData(enemyUnit.monster);
 
         dialogueBox.setAbilityNames(playerUnit.monster.Abilities);
-        setDetails();
+        BattleButton battleButton = new BattleButton();
+        battleButton.setDetails();
 
         yield return StartCoroutine(dialogueBox.typeDialogue($"A Wild {enemyUnit.monster.Base.name} Appeared"));
         yield return new WaitForSeconds(1f);
@@ -43,39 +44,41 @@ public class BattleSystem : MonoBehaviour
     }
 
     //Button Functionalities
-    List<Ability> abilities;
-    private bool clicked = false;
-    
-    private void setDetails() {
-        abilities = playerUnit.monster.Abilities;
-    }
-    public void fightClicked() {
-        dialogueBox.EnableaAbilitySelector(true);
-        dialogueBox.EnableActionSelector(false);
-        dialogueBox.EnableDialogueText(false);
-    }
-
-    public void runClicked() {
-        Debug.Log("Run");
-    }
-
-    public void move1Clicked() {
-        clicked = !clicked;
-        if(!clicked) {
-            Debug.Log("Move1");
+    public class BattleButton : BattleSystem {
+        List<Ability> abilities;
+        private bool clicked = false;
+        
+        public void setDetails() {
+            abilities = playerUnit.monster.Abilities;
         }
-        dialogueBox.setDetailText(abilities[0].Base.getDesc(), abilities[0].Base.getCool());
-    }
-    
-    public void move2Clicked() {
-        Debug.Log("Move2");
-    }
-    
-    public void move3Clicked() {
-        Debug.Log("Move3");
-    }
-    
-    public void move4Clicked() {
-        Debug.Log("Move4");
+        public void fightClicked() {
+            dialogueBox.EnableaAbilitySelector(true);
+            dialogueBox.EnableActionSelector(false);
+            dialogueBox.EnableDialogueText(false);
+        }
+
+        public void runClicked() {
+            Debug.Log("Run");
+        }
+
+        public void move1Clicked() {
+            clicked = !clicked;
+            if(!clicked) {
+                Debug.Log("Move1");
+            }
+            dialogueBox.setDetailText(abilities[0].Base.getDesc(), abilities[0].Base.getCool());
+        }
+        
+        public void move2Clicked() {
+            Debug.Log("Move2");
+        }
+        
+        public void move3Clicked() {
+            Debug.Log("Move3");
+        }
+        
+        public void move4Clicked() {
+            Debug.Log("Move4");
+        }
     }
 }
